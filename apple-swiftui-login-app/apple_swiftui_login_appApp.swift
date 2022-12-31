cd11fd16e4e5f9a0fd6d2f3dd086e4cd90c041e4
themaxermister <subscribingalerts@gmail.com>
2022-12-31 14:37:48 +0800

    Initial Commit

---
+++ apple-swiftui-login-app/apple_swiftui_login_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_swiftui_login_appApp.swift
+//  apple-swiftui-login-app
+//
+//  Created by Jeremy Chua on 31/12/22.
+//
+
+import SwiftUI
+
+@main
+struct apple_swiftui_login_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
