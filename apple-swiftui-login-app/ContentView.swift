b07381193bd44d48a2e429069341b91e01f8d11c
themaxermister <subscribingalerts@gmail.com>
2023-01-17 23:02:06 +0800

    INIT

--- apple-swiftui-login-app/ContentView.swift
+++ apple-swiftui-login-app/ContentView.swift
@@ -7,15 +7,99 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct InputTextField: View {
+    @Binding var stateBinding: String
+    let label: String
+    let placeholder: String
+    let secure: Bool
+    
+    /*
+    init(label: String, placeholtring){
+        self.label = label
+        self.placeholder = placeholder
+        self.secure = false
+    }
+    
+    init(label: String, placeholder:String, secure: Bool){
+        self.label = label
+        self.placeholder = placeholder
+        self.secure = secure
+    }*/
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading){
+            Text(label)
+                .font(.headline)
+                .foregroundColor(.red)
+            
+            if (secure) {
+                SecureField(placeholder, text: $stateBinding)
+                    .textFieldStyle(.roundedBorder)
+            }
+            else{
+                TextField(placeholder, text: $stateBinding)
+                    .textFieldStyle(.roundedBorder)
+            }
         }
-        .padding()
+    }
+}
+
+
+struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+   
+    var body: some View {
+        VStack(alignment: .center){
+            Spacer() // Extends into white space
+            HStack{
+                Spacer()
+                Image("UserIcon")
+                //.resizable().clipShape(Circle())
+                /*.frame(width: 200, height: 200)
+                 .clipped()
+                 .cornerRadius(30) //Interchange resizable*/
+                    .resizable()
+                    .padding()
+                    .frame(width: 200, height: 200)
+                    .aspectRatio (CGSize(width: 315, height: 502), contentMode: .fit)
+                Spacer()
+            }
+            
+            InputTextField(stateBinding: $username, label: "Username", placeholder: "Enter username", secure: false)
+            InputTextField(stateBinding: $password, label: "Password", placeholder: "Enter password", secure: true)
+            
+            
+            HStack(alignment: .center){
+                Button(action: {
+                    print("\(self.username) and \(self.password)")
+                }){
+                    Spacer()
+                    HStack(alignment: .center){
+                        Spacer()
+                        Text("Login")
+                            .font(.headline)
+                            .foregroundColor(.mint)
+                            .multilineTextAlignment(.center)
+                        Spacer()
+                    }
+                    .padding(.vertical, 5)
+                    .background(.black)
+                    .cornerRadius(50)
+                    .frame(width: 100)
+                    Spacer()
+                }
+                .padding(.vertical, 10)
+            }
+            
+            Spacer() // Extends into white space
+        }
+        .padding(.horizontal)
+        .background(Image("BackgroundWallpaper")
+            .resizable()
+            .scaledToFill()
+            .clipped())
+        .edgesIgnoringSafeArea([.top, .bottom])
     }
 }
 
